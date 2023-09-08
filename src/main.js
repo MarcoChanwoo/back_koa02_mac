2e4db5894f0a9c607c771f5a46777cb79c10fc79
Marco <cwhong8087@gmail.com>
2023-09-08 11:42:58 +0900

    10th commit-register/logout

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
+import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
+
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 생성
 const { PORT, MONGO_URI } = process.env;
 
@@ -16,8 +19,6 @@ mongoose // MongoDB 서버와의 연결
         console.error(e);
     });
 
-import api from './api';
-
 const app = new Koa();
 const router = new Router();
 
@@ -26,6 +27,7 @@ router.use('/api', api.routes()); // api 라우트 적용
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
