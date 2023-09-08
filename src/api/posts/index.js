e2bca3a48cfaddd54a1bfc41237d46905891d5f1
Marco <cwhong8087@gmail.com>
2023-09-08 09:04:32 +0900

    4th commit

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -7,7 +7,7 @@ posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
+// posts.put('/:id', postsCtrl.replace); // 구현하지 않을 예정이므로 제거함
 posts.patch('/:id', postsCtrl.update);
 
 export default posts;
